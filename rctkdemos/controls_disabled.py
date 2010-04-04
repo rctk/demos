b16cf18ca60ce8dcd92ee58345c6c69d1c26a2cc
Ivo van der Wijk <github@in.m3r.nl>
2010-04-04 18:36:54 +0000

    demonstrate that, and how, controls can be disabled
    git-svn-id: https://rctk.googlecode.com/svn/demos/trunk@183 286bb87c-ec97-11de-a004-2f18c49ebcc3

---
+++ rctkdemos/controls_disabled.py
@@ -0,0 +1,24 @@
+from demos import serve_demo, standalone
+from rctk.widgets import Button, CheckBox
+
+class Demo(object):
+    title = "Disabled"
+    description = "Demonstrates disabled controls"
+
+    def build(self, tk, parent):
+        button = Button(tk, "Click me!")
+        c = CheckBox(tk)
+
+        def toggle(x):
+            button.state = not button.state
+        c.click = toggle
+
+        button.state = Button.DISABLED
+        parent.append(button)
+        parent.append(c)
+
+Standalone = standalone(Demo)
+
+if __name__ == '__main__':
+    serve_demo(Demo)
+
