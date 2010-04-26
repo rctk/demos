c6ed56524511da3a16b58ea1f1bd3490fce30945
Ivo van der Wijk <github@in.m3r.nl>
2010-04-26 11:01:21 +0000

    fill standalone tabs
    git-svn-id: https://rctk.googlecode.com/svn/demos/trunk@210 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctkdemos/demos.py
+++ rctkdemos/demos.py
@@ -1,15 +1,29 @@
-from rctk.widgets import Window, StaticText, Panel
+from rctk.widgets import Window, StaticText, Panel, StaticHTMLText
 
-from all import DemoPanel
+from all import DemoPanel, make_py
+
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+
+import sys
 
 class DemoRunner(object):
     def __init__(self, demo_class):
         self.demo = demo_class()
 
+    def load_source(self, klass):
+        mod = sys.modules[klass.__module__]
+        sourcecode = open(make_py(mod.__file__), "r").read()
+        return highlight(sourcecode, PythonLexer(), HtmlFormatter(noclasses=True))
+
     def run(self, tk):
         p = DemoPanel(tk)
         tk.root().append(p)
         self.demo.build(tk, p.buildpanel)
+        p.sourcepanel.append(StaticHTMLText(tk, self.load_source(self.demo)))
+        p.descriptionpanel.append(StaticHTMLText(tk, self.demo.description))
+
 
 def serve_demo(demo_class):
     serve(DemoRunner, demo_class)
