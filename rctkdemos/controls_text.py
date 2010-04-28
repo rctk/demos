34f9e83c2d98c7fc81b4aa514221b04ffe2ccbbc
Arjen Roodselaar <arjenroodselaar@gmail.com>
2010-04-28 17:26:50 +0000

    Multi row text fields, text demo standalone
    git-svn-id: https://rctk.googlecode.com/svn/demos/trunk@224 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctkdemos/controls_text.py
+++ rctkdemos/controls_text.py
@@ -1,13 +1,19 @@
-from demos import serve_demo
+from demos import serve_demo, standalone
 from rctk.widgets import Text
+from rctk.layouts import VBox
 
 class Demo(object):
     title = "Text"
     description = "Demonstrates the Text control"
 
     def build(self, tk, parent):
-        text = Text(tk) # , "Text allows you to input text")
-        parent.append(text)
+        parent.setLayout(VBox())
+        text1 = Text(tk) # , "Text allows you to input text")
+        text2 = Text(tk, rows=10, columns=40)
+        parent.append(text1)
+        parent.append(text2)
+    
+Standalone = standalone(Demo)
 
 if __name__ == '__main__':
     serve_demo(Demo)
