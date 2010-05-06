6058768add5db9e9c434e74839b4be7dcf387cb7
Arjen Roodselaar <arjenroodselaar@gmail.com>
2010-05-06 13:15:33 +0000

    Very basic image control
    git-svn-id: https://rctk.googlecode.com/svn/demos/trunk@233 286bb87c-ec97-11de-a004-2f18c49ebcc3

---
+++ rctkdemos/controls_image.py
@@ -0,0 +1,17 @@
+from demos import serve_demo, standalone
+from rctk.widgets import Image
+
+class Demo(object):
+    title = "Image"
+    description = "Demonstrates the Image control"
+
+    def build(self, tk, parent):
+        img = Image(tk, 'http://www.gstatic.com/codesite/ph/images/defaultlogo.png', title='Google Code')
+        parent.append(img)
+    
+    
+Standalone = standalone(Demo)
+
+if __name__ == '__main__':
+    serve_demo(Demo)
+
