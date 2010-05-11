22b6a83146c32096452a4be755cd6421a1753757
Ivo van der Wijk <github@in.m3r.nl>
2010-05-11 20:42:12 +0000

    need it to be standalone runnable
    git-svn-id: https://rctk.googlecode.com/svn/demos/trunk@238 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctkdemos/controls_statictext.py
+++ rctkdemos/controls_statictext.py
@@ -1,4 +1,4 @@
-from demos import serve_demo
+from demos import serve_demo, standalone
 from rctk.widgets import StaticText
 
 class Demo(object):
@@ -8,6 +8,8 @@ class Demo(object):
     def build(self, tk, parent):
         static = StaticText(tk, "StaticText just display some text")
         parent.append(static)
+
+Standalone = standalone(Demo)
 
 if __name__ == '__main__':
     serve_demo(Demo)
