17df18bffb4fb9d10ef8cdc765a16342a6f62675
Arjen Roodselaar <arjenroodselaar@gmail.com>
2010-06-09 08:56:27 +0000

    Very basic Collection control. Layout of the control and it's parents seems somewhat broken (see demo).
    git-svn-id: https://rctk.googlecode.com/svn/demos/trunk@316 286bb87c-ec97-11de-a004-2f18c49ebcc3

---
+++ rctkdemos/controls_collection.py
@@ -0,0 +1,52 @@
+from rctkdemos.demos import serve_demo, standalone
+from rctk.widgets import Text, Button, StaticText, Panel, Collection
+from rctk.layouts import Grid
+
+
+class Demo(object):
+    title = "Collection"
+    description = "Demonstrates the Collection control"
+    
+    def append(self, event):
+        if len(self.text.value):
+            self.collection.append(self.text.value)
+            self.text.value = ''
+            self.collection.layout()
+    
+    def remove(self, event):
+        if len(self.text.value):
+            self.collection.remove(self.text.value)
+            self.text.value = ''
+            self.collection.layout()
+    
+    def clear(self, event):
+        self.collection.clear()
+        self.text.value = ''
+        self.collection.layout()
+    
+    def build(self, tk, parent):
+        parent.setLayout(Grid(rows=2, columns=6))
+        self.text = Text(tk)
+        append_button = Button(tk, 'Append')
+        append_button.click = self.append
+        remove_button = Button(tk, 'Remove')
+        remove_button.click = self.remove
+        clear_button = Button(tk, 'Clear')
+        clear_button.click = self.clear
+        parent.append(self.text)
+        parent.append(append_button)
+        parent.append(StaticText(tk, ' or '))
+        parent.append(remove_button)
+        parent.append(StaticText(tk, ' or '))
+        parent.append(clear_button)
+        
+        self.collection = Collection(tk, StaticText)
+        self.collection.extend(['Hello', 'World', 'Bye'])
+        parent.append(self.collection)
+    
+
+Standalone = standalone(Demo)
+
+if __name__ == '__main__':
+    serve_demo(Demo)
+
