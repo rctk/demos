06b25328f4be19f65ceb75cc5b40247cd89c1f66
Ivo van der Wijk <github@in.m3r.nl>
2010-08-09 14:27:17 +0000

    test and demonstrate the multiple functionality
    git-svn-id: https://rctk.googlecode.com/svn/demos/trunk@359 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctkdemos/controls_list.py
+++ rctkdemos/controls_list.py
@@ -1,15 +1,44 @@
-from rctkdemos.demos import serve_demo
-from rctk.widgets import List
+from rctkdemos.demos import serve_demo, standalone
+from rctk.widgets import List, StaticText
+from rctk.layouts import Grid
 
 class Demo(object):
     title = "List"
     description = "Demonstrates the List control"
 
     def build(self, tk, parent):
+        parent.setLayout(Grid(rows=3, columns=2))
+        parent.append(StaticText(tk, "Single"))
+        parent.append(StaticText(tk, "Multiple"))
         l = List(tk, ((1, "Hello"), (2, "World"), (3, "Bye")))
+        l2 = List(tk, ((1, "One"), (2, "Two"), (3, "Three"), (4, "Four"), (5, "Five")), multiple=True)
         parent.append(l)
+        parent.append(l2)
+
+        out1 = StaticText(tk, str(l.value))
+        out2 = StaticText(tk, str(l2.value))
+
+        def click1(event):
+            out1.text = str(l.value)
+        def click2(event):
+            out2.text = str(l2.value)
+
+        l.click = click1
+        l2.click = click2
+        parent.append(out1)
+        parent.append(out2)
+
+        ## Add some extra's
         l.add(4, "almost last one")
         l.add(5, "Last one")
+        ## make selection
+        l.value = 3
+        l2.value = [1, 3, 5]
+
+        out1.text = str(l.value)
+        out2.text = str(l2.value)
+
+Standalone = standalone(Demo)
 
 if __name__ == '__main__':
     serve_demo(Demo)
