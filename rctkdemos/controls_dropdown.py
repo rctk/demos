41e26e5bc271ec9310dee2769caa960c4cae19a4
Ivo van der Wijk <github@in.m3r.nl>
2011-06-21 22:00:05 +0000

    allow to run standalone
    git-svn-id: https://rctk.googlecode.com/svn/demos/trunk@584 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctkdemos/controls_dropdown.py
+++ rctkdemos/controls_dropdown.py
@@ -1,4 +1,4 @@
-from rctkdemos.demos import serve_demo
+from rctkdemos.demos import serve_demo, standalone
 from rctk.widgets import Dropdown
 
 class Demo(object):
@@ -9,6 +9,8 @@ class Demo(object):
         dropdown = Dropdown(tk, ((1, "Hello"), (2, "World"), (3, "Bye")))
         parent.append(dropdown)
         dropdown.add(4, "Last one")
+
+Standalone = standalone(Demo)
 
 if __name__ == '__main__':
     serve_demo(Demo)
