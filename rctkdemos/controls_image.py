7e0d848735d4df570d2efab0541b3e14c693e6e4
Ivo van der Wijk <github@in.m3r.nl>
2011-08-21 19:36:01 +0000

    make demo start/stoppable, avoids unneccessary load on browser/demoserver
    git-svn-id: https://rctk.googlecode.com/svn/demos/trunk@623 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctkdemos/controls_image.py
+++ rctkdemos/controls_image.py
@@ -1,7 +1,5 @@
-import os
-
 from rctkdemos.demos import serve_demo, standalone
-from rctk.widgets import Image
+from rctk.widgets import Image, Button
 from rctk.resourceregistry import addResource, FileResource
 
 class Demo(object):
@@ -9,20 +7,36 @@ class Demo(object):
     description = "Demonstrates the Image control"
 
     def build(self, tk, parent):
+        self.running = False
+
         resources = [ 
             addResource(FileResource("resources/nyancat-%d.png" % i,
                                      name="nyancat-%d" % i,
                                      type="image/png"))
             for i in range(0, 12)]
         img = Image(tk, resource=resources[0])
+        button = Button(tk, "Start")
         parent.append(img)
+        parent.append(button)
 
         def update_image(i):
-            img.resource = resources[i]
-            tk.set_timer(lambda e: update_image((i+1) % 12), 250)
+            if self.running:
+                img.resource = resources[i]
+                tk.set_timer(lambda e: update_image((i+1) % 12), 250)
 
-        update_image(0)
-    
+        def stop(e):
+            self.running = False
+            button.text = "Start"
+            button.click = start
+
+        def start(e):
+            self.running = True
+            button.click = stop
+            button.text = "Stop"
+            update_image(0)
+
+        button.click = start
+
 Standalone = standalone(Demo)
 
 if __name__ == '__main__':
