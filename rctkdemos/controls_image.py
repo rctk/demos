7a7fec75b172799cc6c3d0149881b5a45153f773
Ivo van der Wijk <github@in.m3r.nl>
2011-08-21 19:07:57 +0000

    Make image demo more exciting. And more current ;)
    git-svn-id: https://rctk.googlecode.com/svn/demos/trunk@620 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctkdemos/controls_image.py
+++ rctkdemos/controls_image.py
@@ -1,15 +1,32 @@
 import os
 
 from rctkdemos.demos import serve_demo, standalone
-from rctk.widgets import ImageFile
+from rctk.widgets import Image
+from rctk.resourceregistry import addResource, FileResource
 
 class Demo(object):
     title = "Image"
     description = "Demonstrates the Image control"
 
+    def __init__(self):
+        self.counter = 0
+        self.resources = []
+
+    def h(self, e):
+        self.counter += 1
+        self.img.resource = self.resources[self.counter % 12]
+        self.tk.set_timer(self.h, 250)
+
     def build(self, tk, parent):
-        img = ImageFile(tk, os.path.join(os.path.dirname(__file__), 'resources/googlecodelogo.png'), title='Google Code')
-        parent.append(img)
+        self.tk = tk
+        for i in range(0, 12):
+            r = addResource(FileResource("resources/nyancat-%d.png" % i,
+                                         name="nyancat-%d" % i,
+                                         type="image/png"))
+            self.resources.append(r)
+        self.img = Image(tk, resource=self.resources[0])
+        parent.append(self.img)
+        self.tk.set_timer(self.h, 250)
     
     
 Standalone = standalone(Demo)
