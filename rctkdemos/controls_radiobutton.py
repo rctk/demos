65587a8c203fec1a872557c8255ce777bc32259b
Ivo van der Wijk <github@in.m3r.nl>
2011-08-23 11:58:47 +0000

    working radiobutton/group demo
    git-svn-id: https://rctk.googlecode.com/svn/demos/trunk@637 286bb87c-ec97-11de-a004-2f18c49ebcc3

--- rctkdemos/controls_radiobutton.py
+++ rctkdemos/controls_radiobutton.py
@@ -1,29 +1,31 @@
 from rctkdemos.demos import serve_demo, standalone
-from rctk.widgets import CheckBox, CheckBoxGroup, StaticText
-from rctk.layouts import HBox
+from rctk.widgets import RadioButton, RadioGroup, StaticText
+from rctk.layouts import VBox
 
 class Demo(object):
     title = "CheckBox"
     description = "Demonstrates the CheckBox control"
 
     def build(self, tk, parent):
-        parent.setLayout(HBox())
-        box1 = CheckBox(tk)
-        group1 = CheckBoxGroup(tk)
-        box2 = CheckBox(tk, checked=True, value="Option 1", group=group1)
-        box3 = CheckBox(tk, group=group1, value="Option 2")
-        box4 = CheckBox(tk, group=group1, value="Option 3")
-        parent.append(box1)
-        parent.append(box2)
-        parent.append(box3)
-        parent.append(box4)
-        box1.checked = True
+        parent.setLayout(VBox())
+        b1 = RadioButton(tk, text="Option 1", value=1)
+        b2 = RadioButton(tk, text="Option 2", value=2)
+        b3 = RadioButton(tk, text="Option 3", value=3)
+        b4 = RadioButton(tk, text="Option 4", value=4)
+        parent.append(b1)
+        parent.append(b2)
+        parent.append(b3)
+        parent.append(b4)
 
-        selection = StaticText(tk, text=group1.value)
-        parent.append(selection)
         def handle_group(e):
-            selection.text = group1.value
-        group1.click = handle_group
+            selection.text = "Selected: %d" % group.value
+        group = RadioGroup(b1, b2, b3, b4)
+        group.selected = b3
+        group.click = handle_group
+
+        selection = StaticText(tk, text="Selected: %d" % group.value)
+        parent.append(selection)
+
 
 
 if __name__ == '__main__':
